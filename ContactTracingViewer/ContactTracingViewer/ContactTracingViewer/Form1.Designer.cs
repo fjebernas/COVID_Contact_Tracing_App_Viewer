c0d857d006add8483d8772cec30ce9b505d0db68
francisjosephironman <francis.3.6.joseph@gmail.com>
2021-12-02 21:09:49 +0800

    Updated UI design to flat style.

--- ContactTracingViewer/ContactTracingViewer/ContactTracingViewer/Form1.Designer.cs
+++ ContactTracingViewer/ContactTracingViewer/ContactTracingViewer/Form1.Designer.cs
@@ -38,6 +38,10 @@ namespace ContactTracingViewer
             this.label2 = new System.Windows.Forms.Label();
             this.btnSearch = new System.Windows.Forms.Button();
             this.labelSearchWarning = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // listBox
@@ -53,8 +57,10 @@ namespace ContactTracingViewer
             // btnViewData
             // 
             this.btnViewData.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.btnViewData.FlatAppearance.BorderSize = 0;
+            this.btnViewData.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnViewData.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnViewData.Location = new System.Drawing.Point(173, 327);
+            this.btnViewData.Location = new System.Drawing.Point(172, 15);
             this.btnViewData.Name = "btnViewData";
             this.btnViewData.Size = new System.Drawing.Size(94, 40);
             this.btnViewData.TabIndex = 4;
@@ -67,7 +73,7 @@ namespace ContactTracingViewer
             this.labelTitle1.AutoSize = true;
             this.labelTitle1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelTitle1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.labelTitle1.Location = new System.Drawing.Point(99, 22);
+            this.labelTitle1.Location = new System.Drawing.Point(101, 22);
             this.labelTitle1.Name = "labelTitle1";
             this.labelTitle1.Size = new System.Drawing.Size(223, 24);
             this.labelTitle1.TabIndex = 5;
@@ -78,7 +84,7 @@ namespace ContactTracingViewer
             this.labelTitle2.AutoSize = true;
             this.labelTitle2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelTitle2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.labelTitle2.Location = new System.Drawing.Point(147, 51);
+            this.labelTitle2.Location = new System.Drawing.Point(149, 51);
             this.labelTitle2.Name = "labelTitle2";
             this.labelTitle2.Size = new System.Drawing.Size(137, 24);
             this.labelTitle2.TabIndex = 5;
@@ -89,7 +95,7 @@ namespace ContactTracingViewer
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.ForeColor = System.Drawing.Color.Black;
             this.label1.Location = new System.Drawing.Point(54, 149);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(105, 17);
@@ -109,7 +115,7 @@ namespace ContactTracingViewer
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.ForeColor = System.Drawing.Color.Black;
             this.label2.Location = new System.Drawing.Point(54, 105);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(97, 17);
@@ -119,6 +125,8 @@ namespace ContactTracingViewer
             // btnSearch
             // 
             this.btnSearch.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnSearch.FlatAppearance.BorderSize = 0;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSearch.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSearch.Location = new System.Drawing.Point(298, 124);
             this.btnSearch.Name = "btnSearch";
@@ -139,26 +147,50 @@ namespace ContactTracingViewer
             this.labelSearchWarning.Size = new System.Drawing.Size(0, 16);
             this.labelSearchWarning.TabIndex = 6;
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.labelTitle1);
+            this.panel1.Controls.Add(this.labelTitle2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(435, 92);
+            this.panel1.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel2.Controls.Add(this.btnViewData);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 329);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(435, 76);
+            this.panel2.TabIndex = 10;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.ClientSize = new System.Drawing.Size(435, 405);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.txtBxSearch);
             this.Controls.Add(this.labelSearchWarning);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.labelTitle2);
-            this.Controls.Add(this.labelTitle1);
-            this.Controls.Add(this.btnViewData);
             this.Controls.Add(this.listBox);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Contact Tracing App Data Viewer";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Click += new System.EventHandler(this.Form1_Click);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -174,6 +206,8 @@ namespace ContactTracingViewer
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.Label labelSearchWarning;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
